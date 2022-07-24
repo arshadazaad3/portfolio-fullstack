2a0e6c9e3269ffe43ffd7122bb5f975c9ba636b0
arshadazaad3 <arshad.azaad32@gmail.com>
2022-07-24 18:37:35 +0800

    Header changes

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React from 'react';
+import { motion } from 'framer-motion';
 
-import "./Header.scss";
+import { images } from '../../constants';
+import './Header.scss';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="app__header app_flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-batch">
+          <div className="batch-cmb app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Root</h1>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      Header
+    </div>
+  );
 }
