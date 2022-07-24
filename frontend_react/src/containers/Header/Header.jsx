80eb48373dd4d2dcf380a0d5efbd44c117ac382b
arshadazaad3 <arshad.azaad32@gmail.com>
2022-07-24 23:28:52 +0800

    Header changes

--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -6,7 +6,7 @@ import './Header.scss';
 
 export default function Header() {
   return (
-    <div className="app__header app_flex">
+    <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -20,9 +20,30 @@ export default function Header() {
               <h1 className="head-text">Root</h1>
             </div>
           </div>
+
+          <div className="tag-cmb app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Freelancer</p>
+          </div>
         </div>
       </motion.div>
-      Header
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile-bg" />
+        <motion.img
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+
+      <motion.div></motion.div>
     </div>
   );
 }
